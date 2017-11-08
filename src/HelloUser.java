81601cc9541b8a39700a5d5ae98f1f405b8bbc0d
Kadir Tilki <kadir.tilki@tum.de>
2017-11-08 10:33:33 +0100

    HelloUser is added

---
+++ src/HelloUser.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+	
+public class HelloUser {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello User - How are you? :)");
+	}
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String line=buffer.readLine();
+			System.out.println(line);
+		}catch(Exception e)
+		{
+			
+		}
 	}
 
 }
