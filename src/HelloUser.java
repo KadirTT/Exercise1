cc765f476e746f7f06396df000d0349d59f1b056
Kadir Tilki <kadir.tilki@tum.de>
2017-11-08 12:45:44 +0100

    removed unnecesary lines

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-	
-public class HelloUser {
+public class HelloUser 
+{
 	private String userName;
 	
 	public HelloUser(String uName) {
@@ -11,16 +9,6 @@ public class HelloUser {
 	public void greetUser()
 	{
 		System.out.println("Hello " + userName + "!");
-				
-		BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
-		try
-		{
-			String line=buffer.readLine();
-		}
-		catch(Exception e)
-		{
-			
-		}
 	}
 
 }
